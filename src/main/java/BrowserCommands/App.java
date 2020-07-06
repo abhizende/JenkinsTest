fa947fa98745c7100969f677bd2f36fac105d9af
abhizende <abhizende@gmail.com>
2020-07-06 12:03:09 +0530

    first commit

---
+++ src/main/java/BrowserCommands/App.java
@@ -0,0 +1,18 @@
+package BrowserCommands;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	@Test
+	public void App() 
+	{
+		System.out.println("Hello World!");
+    
+	}
+
+}
